9ee23c7d10b792523cfa888000b979b15538d663
jessicap5159 <jessicaj5159@gmail.com>
2021-06-30 11:22:09 -0700

    created typedef for user

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,21 @@
 const { gql } = require('apollo-server-express');
 
 // create typeDefs
-const typeDefs = gql ``;
+const typeDefs = gql `
+type User {
+    _id: ID
+    username: String
+    email: String
+    projectCount: Int
+    taskCount: Int
+    projects: [Project]
+    tasks: [Task]
+
+    
+}
+
+`;
 
 // export typeDefs
 module.exports = typeDefs;
+
