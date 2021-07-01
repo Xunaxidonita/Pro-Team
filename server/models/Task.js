2dc5b08f9f5b175312a47f707d92fd971a94443f
jessicap5159 <jessicaj5159@gmail.com>
2021-07-01 11:35:43 -0700

    wrote task model

---
+++ server/models/Task.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const taskSchema = new Schema(
+  {
+    taskName: {
+      type: String,
+      required: true,
+      maxlength: 140,
+    },
+    taskText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    assignedTo: {
+      type: String,
+      maxlength: 20,
+    },
+    dueDate: {
+        type: Date
+        
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+module.exports = reactionSchema;
