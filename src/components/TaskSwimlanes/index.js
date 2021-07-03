dad6135ee8d408ba842306caa52f572fe8aa232b
jessicap5159 <jessicaj5159@gmail.com>
2021-07-03 15:42:29 -0700

    created task swimlanes for my tasks

--- src/components/TaskSwimlanes/index.js
+++ src/components/TaskSwimlanes/index.js
@@ -37,4 +37,32 @@ const TaskSwimlanesAll = () => (
   </Table>
 )
 
+// Not sure if this should go here:
+
+const TaskSwimlanesMyTasks = () => (
+    <Table basic>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>In Progress:</Table.HeaderCell>
+          <Table.HeaderCell>Completed:</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+  
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>Placeholder Task 1</Table.Cell>
+          {/* *due dates! */}
+          <Table.Cell>Placeholder Task 2</Table.Cell>
+          <Table.Cell>Placeholder Task 3</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Placeholder Task 1</Table.Cell>
+          <Table.Cell>Placeholder Task 2</Table.Cell>
+          <Table.Cell>Placeholder Task 3</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  )
+
 export default TaskSwimlanesAll;
+export default TaskSwimlanesMyTasks; 
