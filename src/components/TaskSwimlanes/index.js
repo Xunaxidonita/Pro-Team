1f47b517cf0b89d3137da88f5ac66795964c37ce
jessicap5159 <jessicaj5159@gmail.com>
2021-07-03 15:38:50 -0700

    created task swimlanes for all tasks

---
+++ src/components/TaskSwimlanes/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Table } from 'semantic-ui-react'
+
+const TaskSwimlanesAll = () => (
+  <Table basic>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Unassigned:</Table.HeaderCell>
+        <Table.HeaderCell>Assigned:</Table.HeaderCell>
+        <Table.HeaderCell>In Progress:</Table.HeaderCell>
+        <Table.HeaderCell>Completed:</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>Placeholder Task 1</Table.Cell>
+        <Table.Cell>Placeholder Task 2</Table.Cell>
+        <Table.Cell>Placeholder Task 3</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Placeholder Task 1</Table.Cell>
+        <Table.Cell>Placeholder Task 2</Table.Cell>
+        <Table.Cell>Placeholder Task 3</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Placeholder Task 1</Table.Cell>
+        <Table.Cell>Placeholder Task 2</Table.Cell>
+        <Table.Cell>Placeholder Task 3</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Placeholder Task 1</Table.Cell>
+        <Table.Cell>Placeholder Task 2</Table.Cell>
+        <Table.Cell>Placeholder Task 3</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+)
+
+export default TaskSwimlanesAll;
