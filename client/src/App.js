987d78f68adb19f3da3d5bd8f4deb12709803c21
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-04 12:23:47 -0700

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import LoginPage from './components/LoginPage';
-
+import ProjectsPage from './components/ProjectsPage';
+import NewProjectPage from './components/NewProjectsPgae'
 function App() {
   return (
-    <LoginPage></LoginPage>
+
+    <Switch>
+     <Route exact path="/">
+          <LoginPage></LoginPage>
+          </Route>
+          <Route path="/projects">
+            <ProjectsPage />
+          </Route>
+          <Route path="/projects/new">
+            <NewProjectPage />
+          </Route>
+   </Switch>
+    
   );
 }
 
