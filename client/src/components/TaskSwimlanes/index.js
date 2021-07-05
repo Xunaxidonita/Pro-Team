5a737d40a5e60e83ba9b30f8d951d3ea8bcb7bb5
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-05 16:07:44 -0700

    render viewer

--- client/src/components/TaskSwimlanes/index.js
+++ client/src/components/TaskSwimlanes/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Table } from 'semantic-ui-react'
+import React from "react";
+import { Table } from "semantic-ui-react";
 
 const TaskSwimlanesAll = () => (
   <Table basic>
@@ -35,34 +35,8 @@ const TaskSwimlanesAll = () => (
       </Table.Row>
     </Table.Body>
   </Table>
-)
+);
 
 // Not sure if this should go here:
 
-const TaskSwimlanesMyTasks = () => (
-    <Table basic>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>In Progress:</Table.HeaderCell>
-          <Table.HeaderCell>Completed:</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-  
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>Placeholder Task 1</Table.Cell>
-          {/* *due dates! */}
-          <Table.Cell>Placeholder Task 2</Table.Cell>
-          <Table.Cell>Placeholder Task 3</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Placeholder Task 1</Table.Cell>
-          <Table.Cell>Placeholder Task 2</Table.Cell>
-          <Table.Cell>Placeholder Task 3</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
-  )
-
 export default TaskSwimlanesAll;
-export default TaskSwimlanesMyTasks; 
