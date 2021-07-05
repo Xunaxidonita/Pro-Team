2b4a1bfb3e482fa6c205e960b17e7a6ab26e5994
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-05 16:07:44 -0700

    render viewer

--- client/src/components/ProjectViewer/index.js
+++ client/src/components/ProjectViewer/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import TaskSwimlanesAll from "../TaskSwimlanes";
 import Toggle from "../Toggle";
-import { Label } from "semantic-ui-react";
 
 const ProjectView = () => (
   <>
-    <Label>See all Project Tasks</Label>
     <Toggle></Toggle>
     <TaskSwimlanesAll></TaskSwimlanesAll>
   </>
