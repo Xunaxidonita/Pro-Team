9342a7f15c5b8e2d30df4cf17d84e38c66e0acb5
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-05 22:31:22 -0700

    Fix bugs in NewTask route

--- client/src/components/TaskForm/index.js
+++ client/src/components/TaskForm/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import { Button, Select, Form } from "semantic-ui-react";
 
 const TaskForm = ({ project }) => {
-  const members = ["Johnny Appleseed", "Daniel Boon"];
+  const members = [
+    { key: "ja", value: "ja", text: "Johnny Appleseed" },
+    { key: "db", value: "db", text: "Daniel Boon" },
+  ];
+  const options = [
+    { key: "ag", value: "ag", text: "Assigned" },
+    { key: "ip", value: "ip", text: "In Progress" },
+    { key: "cm", value: "cm", text: "Completed" },
+  ];
   return (
     <Form>
       <Form.Field>
@@ -15,11 +23,11 @@ const TaskForm = ({ project }) => {
       </Form.Field>
       <Form.Field>
         <label>Assignee</label>
-        <select></select>
+        <Select placeholder="Select a programer" options={members}></Select>
       </Form.Field>
       <Form.Field>
         <label>Status</label>
-        <Select placeholder="Select a programer" options={members}></Select>
+        <Select placeholder="Unassigne" options={options}></Select>
       </Form.Field>
       <Button type="submit">Submit</Button>
     </Form>
