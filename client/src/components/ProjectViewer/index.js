94e812f365ea795f17c94b83b0cec5c0227011a7
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-05 18:27:41 -0700

    Task Cards

--- client/src/components/ProjectViewer/index.js
+++ client/src/components/ProjectViewer/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import TaskSwimlanesAll from "../TaskSwimlanes";
 import Toggle from "../Toggle";
+import { Label } from "semantic-ui-react";
 
 const ProjectView = () => (
   <>
+    <Label>See all Project Tasks</Label>
     <Toggle></Toggle>
     <TaskSwimlanesAll></TaskSwimlanesAll>
   </>
 );
+
+export default ProjectView;
---
+++ client/src/components/SignUpPage/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import SignupForm from "../LoginForm";
+const SignUpPage = () => <SignupForm></SignupForm>;
+
+export default SignUpPage;
