a2ce99a5960812b76a108cf42e3bdd80086e17dd
Kristian <56667566+kristian821@users.noreply.github.com>
2021-07-09 18:17:17 -0700

    edited server resolvers and added logout button

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import auth from "../../utils/auth";
 
-const Nav = () => (
-  <Menu>
-    <Menu.Item name="homepage">
-      <Link to="/projects">Homepage</Link>
-    </Menu.Item>
-    <Menu.Menu position="right">
-      <Menu.Item name="logout" content="Logout" />
-    </Menu.Menu>
-  </Menu>
-);
+const Nav = () => {
+
+const logout = event => {
+  event.preventDefault();
+  auth.logout();
+}
+
+  return (
+    <Menu>
+      <Menu.Item name="homepage">
+        <Link to="/projects">Homepage</Link>
+      </Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" content="Logout">
+          <a href="/" onClick={logout}>
+            Logout
+          </a>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+}
 
 export default Nav;
